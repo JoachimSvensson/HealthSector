9b29d8e8abe4fab3a6cb3211dafe8bfc133a50f7
JoachimSvensson <joachim.k.svensson@gmail.com>
2024-02-04 18:19:13 +0100

    salabim trials

--- simulation/sim-salabim.py
+++ simulation/sim-salabim.py
@@ -1,33 +1,51 @@
 import salabim as sim 
 
 
+
 class CustomerGenerator(sim.Component):
     def process(self):
         while True:
-            Customer()
-            self.hold(sim.Uniform(5,15).sample())
+            Customer().enter(waiting_room)
+            self.hold(sim.Uniform(5, 15).sample())
+
 
 class Customer(sim.Component):
-    def process(self):
-        self.enter(waitingline)
-        if clerk.ispassive():
-            clerk.activate()
-        self.passivate()
+    ...
+    # def process(self):
+        # self.enter(waitingline)
+        # for clerk in clerks:
+        #     if clerk.ispassive():
+        #         clerk.activate()
+        #         break
+        # self.passivate()
+
 
 class Clerk(sim.Component):
     def process(self):
         while True:
-            while len(waitingline) == 0:
-                self.passivate()
-            self.customer = waitingline.pop()
+            customer = self.from_store(waiting_room)
             self.hold(30)
-            self.customer.activate()
+            # while len(waitingline) == 0:
+            #     self.passivate()
+            # self.customer = waitingline.pop()
+            # self.hold(30)
+            # self.customer.activate()
 
-env = sim.Environment(Trace=True)
-CustomerGenerator
-clerk = Clerk()
-waitingline = sim.Queue('waitingline')
 
-env.run(till=50)
-print()
-waitingline.print_statistics()
+env = sim.Environment(trace=True)
+
+CustomerGenerator()
+for _ in range(3):
+    Clerk()
+# clerks = [Clerk() for _ in range(3)]
+# waitingline = sim.Queue('waitingline')
+waiting_room = sim.Store('waiting_room')
+
+
+env.run(till=5000)
+# waitingline.print_histograms()
+# waitingline.print_info()
+# print()
+# waitingline.print_statistics()
+waiting_room.print_statistics()
+waiting_room.print_info()
